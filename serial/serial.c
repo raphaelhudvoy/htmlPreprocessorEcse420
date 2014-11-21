98b78a9d0187bc0c20188d1c1c663bfbc13f0279
raphael <raphael.hudon-voyer@mail.mcgill.ca>
2014-11-20 20:42:35 -0500

    asdf

--- serial/serial.c
+++ serial/serial.c
@@ -3,7 +3,6 @@
 FILE *input;            /* declare the file pointer */
 
 
-
 int main() {
 
 	FILE *ptr_file;
@@ -14,9 +13,43 @@ int main() {
 	if (!ptr_file) 
 		return 1;
 
+	int newComponent = 1;
+	char output[1000];
+	int count = 0;
+
 	while (fscanf(ptr_file, "%s", buf) !=EOF) {
-		printf("allo\n");
-       	printf("%s \n", buf);
+
+		switch (buf[0]) {
+			case '#' :
+				
+
+			break;
+
+			case '.' :
+			break;
+
+			case '{':
+			break;
+
+			case '}':
+			break;
+
+			default :
+				// if (buf != ' ') {
+
+				// 	if (newComponent == 1) {
+				// 		output[count] = '<';
+				// 		count++;
+				// 		output[count] = buf;
+
+				// 	}
+
+				// }
+		}
+
+
+       printf("%s\n", buf);
+       printf("%c\n", buf[0]);
 	}
 
 
