acb830c5d4bf209e94ae7f358470597cdd1501f4
raphael <raphael.hudon-voyer@mail.mcgill.ca>
2014-11-20 20:12:07 -0500

    asdfa

--- serial/serial.c
+++ serial/serial.c
@@ -1,1 +1,28 @@
-//
+#include <stdio.h>   /* required for file operations */
+
+FILE *input;            /* declare the file pointer */
+
+
+
+int main() {
+
+	FILE *ptr_file;
+    char buf[1000];
+
+	ptr_file =fopen("input.hnrl", "r");
+
+	if (!ptr_file) 
+		return 1;
+
+	while (fscanf(ptr_file, "%s", buf) !=EOF) {
+		printf("allo\n");
+       	printf("%s \n", buf);
+	}
+
+
+    fclose(ptr_file);
+
+
+	return  0;
+
+}
